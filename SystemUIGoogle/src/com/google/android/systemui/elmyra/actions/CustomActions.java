ezio84/abc_external_google

Active Edge: Add Skip/Previous Song Function [3/3]
Change-Id: I4d5726cf8baf681b52b64ede1412cf842d3cea4e

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -9,6 +9,7 @@ import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 
 import com.android.internal.util.custom.Utils;
 import com.android.systemui.Dependency;
@@ -101,6 +102,12 @@ public class CustomActions extends Action {
                 if (isScreenOn) {
                     Utils.killForegroundApp();
                 }
+                break;
+            case 14: // Skip song
+                Utils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                Utils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
         }
     }

BlissRoms/platform_external_google

SystemUIGoogle/src/com/google/android/systemui/elmyra/actions/CustomActions.java
@@ -9,6 +9,7 @@ import android.os.UserHandle;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 
 import com.android.internal.util.bliss.BlissUtils;
 import com.android.systemui.Dependency;
@@ -96,6 +97,12 @@ public class CustomActions extends Action {
                 if (isScreenOn) {
                     launchApp(getContext(), detectionProperties.isLongSqueeze());
                 }
+                break;
+            case 13: // Skip song
+                BlissUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 14: // Previous song
+                BlissUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
         }
     }